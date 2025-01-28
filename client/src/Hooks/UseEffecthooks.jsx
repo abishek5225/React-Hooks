48d8857e5e843649fc0ca4c3712bd1de873c2fa3
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-01-29 00:33:29 +0545

    updated

--- client/src/Hooks/UseEffecthooks.jsx
+++ client/src/Hooks/UseEffecthooks.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { useEffect } from 'react'
 
 export default function UseEffecthooks() {
+
+ 
   return (
     <div>
-      hi
+      <h1>UseEffect hook</h1>
+      <p>useEffect is a hook that allows you to perform side effects in function components.</p>
+      <h2>I will be increment in every 1 sec</h2>
+      <h3>{}</h3>
     </div>
   )
 }
